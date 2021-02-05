ca74997509af9c50c57145f4faca292f09e7c2eb
安树博 <shuboan@yunify.com>
2021-02-06 02:07:32 +0800

    完善单机节点, 暂时不适用systemd, 在脚本里实现起停

--- ansible/roles/redis-standlone/files/opt/app/current/bin/node/redis.sh
+++ ansible/roles/redis-standlone/files/opt/app/current/bin/node/redis.sh
@@ -9,5 +9,41 @@ initNode() {
 }
 
 measure() {
-  echo '{"connections": '$(netstat -lanp | grep $MY_IP:80 | grep ESTABLISHED | wc -l)'}'
+  echo '{"connections": '$(netstat -ntp | awk -v ip="$MY_IP:$MY_PORT" '$6=="ESTABLISHED" && $5 == ip ' | wc -l)'}'
+}
+
+startSvc() {
+  if [ "${1%%/*}" == "redis-server" ]; then
+    echo "start ${1%%/*}"
+    /opt/redis/current/redis-server /data/redis/conf/redis.conf
+  else
+    echo '_startSvc'
+    _startSvc ${1%%/*}
+  fi
+  touch $APPCTL_CLUSTER_FILE
+  touch $APPCTL_NODE_FILE
+}
+
+stopSvc() {
+  echo "stop ${1%%/*}"
+  if [ "${1%%/*}" == "redis-server" ]; then
+    if [ -f '/data/redis/run/redis.pid' ]; then
+      local mainpid=$(cat /data/redis/run/redis.pid)
+      /bin/kill -15 $mainpid || /bin/kill -9 $mainpid
+      tail --pid=$mainpid -f /dev/null
+    fi
+  else
+    _startSvc ${1%%/*}
+  fi
+}
+
+checkSvc()
+{
+  if [ "${1%%/*}" == "redis-server" ]; then
+    echo "checkSvc redis-server"
+    /opt/redis/current/redis-cli -h 127.0.0.1 -p ${1#*:} ping
+  else
+    _checkSvc ${1}
+  fi
+
 }
